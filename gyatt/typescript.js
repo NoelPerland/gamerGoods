41c983aa0e6a2c016d4811e533c113d924b072c6
Noel Perland <noel.perland@yh.nackademin.se>
2023-09-18 14:21:20 +0200

    Second commit with shadcn

---
+++ gyatt/typescript.js
@@ -0,0 +1,5 @@
+var decimal = 6;
+var myString = "hej";
+// console.log("Alex is good at code!");
+console.log(myString);
+console.log(decimal);
---
+++ gyatt/typescript.ts
@@ -0,0 +1,8 @@
+let decimal: number = 6;
+
+let myString: string = "hej";
+
+// console.log("Alex is good at code!");
+
+console.log(myString);
+console.log(decimal);
